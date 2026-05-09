0ea69f04394bb78324b0c534c5c38965d9a16019
Vishnaka Dasanayaka <vishnakadasanayaka@gmail.com>
2026-05-09 23:18:09 +0530

    Attendace Report initaited

---
+++ src/app/routes/reports/attendance-report/attendance-report.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attendance-report',
+  standalone: false,
+  templateUrl: './attendance-report.component.html',
+  styleUrl: './attendance-report.component.css'
+})
+export class AttendanceReportComponent {
+
+}
